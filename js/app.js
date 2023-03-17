bae298584083f15a66336ce36254a4fda95fa605
Enamul <muhammad.enamul.haque79@gmail.com>
2023-03-17 14:41:05 +0600

    img-slider for future uses

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
-const imgCollection=[
-    'nature-1.jpg',
-    'nature-2.jpg',
-    'nature-3.jpg',
-    'nature-4.jpg',
-    'nature-5.jpg',
-    'nature-6.jpg',
-    'nature-7.jpg',
-    'nature-8.jpg',
-    'nature-9.jpg',
-    'nature-10.jpg',
-    'nature-11.jpg',
-    'nature-12.jpg',
-    'nature-13.jpg',
-    'nature-14.jpg',
-    'nature-15.jpg',    
-]
+const imgCollection =[
+    'images/nature-1.jpg',
+    'images/nature-2.jpg',
+    'images/nature-3.jpg',
+    'images/nature-4.jpg',
+    'images/nature-5.jpg',
+    'images/nature-6.jpg',
+    'images/nature-7.jpg',
+    'images/nature-8.jpg',
+    'images/nature-9.jpg',
+    'images/nature-10.jpg',
+    'images/nature-11.jpg',
+    'images/nature-12.jpg',
+    'images/nature-13.jpg',
+    
+];
+
 let imgIndex = 0;
-setInterval( ()=>{
-    console.log(imgCollection[imgIndex]);
+const imgElement = document.getElementById('slider-img');
+setInterval(() =>{
+    if(imgIndex === imgCollection.length){
+        imgIndex = 0;
+    }
+    const imgUrl = imgCollection[imgIndex];
+    imgElement.setAttribute('src',imgUrl);
+    console.log(imgIndex,imgUrl);
     imgIndex++;
 },1000);
+
